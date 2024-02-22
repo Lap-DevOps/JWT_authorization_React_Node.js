39b5631ca884590b05ae4ce75e8100ceba7b61ac
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-02-22 15:23:16 +0000

    feat: auth middleware

---
+++ server/middlewares/auth-middleware.js
@@ -0,0 +1,19 @@
+const ApiError = require('../exeptions/api-error');
+const tokenService = require('../service/token-service');
+
+module.exports = function (req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return next(ApiError.UnauthorizedError());
+        }
+        const userData = tokenService.validateAccessToken(token);
+        if (!userData) {
+            return next(ApiError.UnauthorizedError());
+        }
+        req.user = userData;
+        next();
+    } catch (e) {
+        return next(ApiError.UnauthorizedError());
+    }
+}
