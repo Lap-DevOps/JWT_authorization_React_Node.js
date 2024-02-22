26a46ba39913c96feed40cf37573103b9f6d832a
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-02-22 13:05:40 +0000

    feat: add login route

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -39,9 +39,29 @@ class UserService {
         }
         user.isActivated = true;
         await user.save();
+    }
 
+    async login(email, password) {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            throw ApiError.BadRequest("User not found")
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.BadRequest("Wrong password")
+        }
+        const userDto = new UserDto(user); // id, email, isActivated
+        const tokens = tokenService.generateTokens({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-}}
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
+    
+
+}
 
 
 
